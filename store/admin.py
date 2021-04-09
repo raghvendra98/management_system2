13fe4a912a89d41d4c7d047e8f1fc361c9789c39
Raghendra kumar <50679225+raghvendra98@users.noreply.github.com>
2021-04-09 18:50:15 +0530

    fix:fix login isuue

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import (
+    Supplier,
+    Buyer,
+    Season,
+    Drop,
+    Product,
+    Order,
+    Delivery
+)
+
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'address', 'created_date']
+
+
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'address', 'created_date']
+
+
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Buyer, BuyerAdmin)
+admin.site.register(Season)
+admin.site.register(Drop)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Delivery)
