11fa9a655e28628ddb6e805a6bfb08e8b1bc4c2a
Raghendra kumar <50679225+raghvendra98@users.noreply.github.com>
2021-04-09 18:48:48 +0530

    fix:fir urls issue

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from users.models import User
 
+
 class Supplier(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=120, unique=True)
