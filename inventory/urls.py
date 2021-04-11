9b70a826924b1a39c4178f977a7b7afe94e46938
Raghendra kumar <50679225+raghvendra98@users.noreply.github.com>
2021-04-11 23:49:37 +0530

    fix:set url

--- inventory/urls.py
+++ inventory/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
     path('users/', include('users.urls')),
-    path('store/', include('store.urls'))
+
 
 ]
